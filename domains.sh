ff04ba4f0c8fbf8790b29ba1de1243b83478db68
Zuan-Pierre <zuan99@gmail.com>
2018-07-22 20:56:51 +0200

    Update domains.sh

--- domains.sh
+++ domains.sh
@@ -4,27 +4,38 @@
 # this will print each domain in the terminal as it looks it up
 # The result csv will contain the domain, IP, MX records & Nameservers in each column
 
-# Give each column the relevant header titles
-echo "Domain Name,IP Address,HTTP Status,MX,MX,Nameserver,Nameserver,Nameserver,Nameserver" > domains.csv
+# Get reseller name from user input
+echo "What would you like to save the CSV as?"
 
-while read domain
+read spreadsheet
+
+# Provide name for spreadsheet
+spreadsheet=$spreadsheet'.csv'
+
+# Give each column the relevant header titles
+echo "Domain Name,IP Address,HTTP Status,Registrar,MX,MX,Nameserver,Nameserver,Nameserver,Nameserver" > $spreadsheet
+
+while read -r domain
 do
   # Get IP address defined in domain's root A-record
-  ipaddress=`dig $domain +short`
+  ipaddress=`dig "$domain" +short`
 
   # Get MX-records
-  mx=`dig mx $domain +short| sort | tr '\n' ','`
+  mx=`dig mx "$domain" +short| sort | tr '\n' ','`
 
   # Get list of all nameservers
-  ns=`dig ns $domain +short| sort | tr '\n' ','`
+  ns=`dig ns "$domain" +short| sort | tr '\n' ','`
 
-  #Get HTTP status code
+  # Get HTTP status code
   http=$( curl -w %{http_code} -s --output /dev/null --max-time 15 $domain)
+
+  # Get Registrar
+  registrar=`whois "$domain" | grep "Registrar:" | cut -d ":" -f 2 |  sed 's/ //' | sed -n '2!p'`
 
   echo "$domain"
 
   # Prints all the values fetched into the CSV file
-  echo -e "$domain,$ipaddress,$http,$mx,$ns" >> domains.csv
+  echo -e "$domain,$ipaddress,$http,$registrar,$mx,$ns" >> $spreadsheet
 
 # Defines the text file from which to read domain names
 done < domains.txt
